7323bcd24654861a96374d196b6f8e597389346b
Gabriel <gabriel.rezende131101@gmail.com>
2026-07-30 20:30:25 -0300

    Estrutura Inicial

---
+++ src/test/java/br/com/video2frames/video2frames_auth_service/Video2framesAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.video2frames.video2frames_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Video2framesAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
